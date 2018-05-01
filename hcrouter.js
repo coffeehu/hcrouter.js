a9ba64828d9d230207f9e9d3b575f00aea23d0e5
Coffeehu <coffeehu@gmail.com>
2018-05-01 11:11:20 +0800

    嵌套路由 nested route

--- hcrouter.js
+++ hcrouter.js
@@ -1,4 +1,4 @@
-/*思路源于 director.js*/
+/*思路参考 director.js*/
 
 (function(exports){
 
@@ -21,24 +21,42 @@ var Router = exports.Router = function(routes){
 	console.log(this.routes)
 }
 
-Router.prototype.mount = function(routes, objPath){
+/*
+	第一次，routes为路由配置对象，objPath为undefined
+	递归时，可能的值为：routes为{on:func,after:func},objPath为对应的路径，如'/home'
+*/
+Router.prototype.mount = function(routes, parentPath){
 	if(!routes || typeof routes !== 'object' || Array.isArray(routes) ){
 		return;
 	}
-	var that = this;
+	var that = this,
+		parentPath = parentPath || [];
 
-	function insertOrMount(path,route,objPath){
-		var isRoute = ( path.split('/')[0]==='' ),
-			routeType = typeof route,
-			method = 'on';
+	function insertOrMount(path,route,parentPath){
+
+		//如果是这种形式："/home/detail"，直接插入 this.routes
+		var unNested = path.split('/').length>2;
+		if(unNested){
+			that.routes[path] = route;
+			return;
+		}
 
 		/*
 			参数 path 可能是路径如"/home",
 			也可能是生命周期如"on"、"before"、"after"
 		*/
+		var isRoute = ( path.split('/')[0]==='' ),
+			routeType = typeof route,
+			method = isRoute?'on':path;
+
+		if(isRoute){
+			path = parentPath.concat( path.split('/') );
+			path = path.filter(Boolean)
+		}
+
 		// 是 路径=>Function 的情况时，直接插入，生命周期默认只有 on
 		if(isRoute && routeType==='function'){
-			that.insert( path, method, routes[path] );
+			that.insert( path, method, route );
 			return;
 		}
 
@@ -51,28 +69,35 @@ Router.prototype.mount = function(routes, objPath){
 
 		//是生命周期=>function的情况时（如："on":function）
 		if(!isRoute && routeType==='function'){
-			method = path;
-			that.insert( objPath, method, routes[path] );
+			that.insert( parentPath, method, route );
 			return;
 		}
 	}
 
 	for(var path in routes){
 		if(routes.hasOwnProperty(path)){
-			insertOrMount(path, routes[path], objPath);
+			insertOrMount(path, routes[path], parentPath.slice(0));
 		}
 	}
 }
 
 //构建路由表
-Router.prototype.insert = function(path,method,fn){
-	path = path.slice(1); // "/home" => "home"
-	if(this.routes[path]){
-		this.routes[path][method] = fn;
+Router.prototype.insert = function(path,method,fn,parentRoute){
+	var route = parentRoute ? parentRoute : this.routes,
+		pathPart = path.shift();
+
+	if(path.length>0){
+		route[pathPart] = route[pathPart] || {};
+		this.insert(path,method,fn,route[pathPart]);
+		return;
+	}
+
+	if(route[pathPart]){
+		route[pathPart][method] = fn;
 	}else{
 		var nested = {};
 		nested[method] = fn;
-		this.routes[path] = nested;	
+		route[pathPart] = nested;
 	}
 }
 
@@ -88,17 +113,79 @@ Router.prototype.init = function(){
 }
 
 //根据 hash 执行对应的回调事件
+//path 为 "home" 或 "home/detail"
+var toOther=0,toNested=1,toParent=2;
 Router.prototype.dispatch = function(path){
-	var runList,route;
-	if(typeof this.routes[path] === 'object'){
-		this.invoke(this.last); //调用上次路由的after
+	var pathString = '/'+path;
 
-		route = this.routes[path];
-		runList = [ route.before, route.on ].filter(Boolean);
-
-		this.last = [ route.after ];
+	// 是 "/home/room" 这种形式的路径
+	// 若能在 this.routes 中直接找到，则直接执行
+	if( pathString.split('/').length>2 && this.routes[pathString]){
+		var runList = [ this.routes[pathString].before, this.routes[pathString].on ];
+		this.invoke(this.last);
+		this.last = [ this.routes[pathString].after ];
+		this.invoke(runList);
+		return;
 	}
-	this.invoke(runList); //存储这次路由的after
+
+	// 转成数组，如 ["home"]、["home","detail"]
+	path = path.split('/');
+	var runList = this.createRunList(path.slice(0),this.routes);
+
+	function typeOfRoute(path,lastPath,parentPath){
+		if( lastPath && lastPath === parentPath){
+			return toNested;
+		}
+		else if( lastPath && lastPath.match( new RegExp(pathString) ) ){
+			return toParent;
+		}
+		else{
+			return toOther;
+		}
+	}
+
+	var type = typeOfRoute(path,this.lastPath,runList.parentPath);
+
+	/*
+	 1、跳到嵌套路由时，如："/home"=>"/home/room" 时，不执行 /home 的 after；
+	 而是将 /home 的 after 存储起来，若是再从 /home/room => /other, 会依次调用/home/room的after和/home的after
+	*/
+	if( type === toNested ){
+		this.last.unshift( runList.after );
+		this.invoke(runList);
+	}else if( type === toParent ){
+		if( this.last && this.last.length>0) this.invoke( [ this.last.shift() ] );
+	}else if( type === toOther ){
+		this.invoke(this.last); //调用上次路由的after	
+		this.last = [ runList.after ];
+		this.invoke(runList);
+	}
+	//this.invoke(runList);
+	this.lastPath = '/'+path.join('/');
+}
+
+//创建执行队列,如：[before,on]
+// path 为数组，如 ["home"] 、['home','detail']
+Router.prototype.createRunList = function(path,routes,parentPath){
+	var runList = [],
+		pathPart,route;
+
+	pathPart = path.shift();
+
+	if(typeof routes[pathPart] === 'object'){
+		if(path.length > 0){ //说明是嵌套路由
+			parentPath = parentPath || ''; //记录其父路径
+			runList = this.createRunList(path, routes[pathPart],parentPath+'/'+pathPart);
+			return runList;
+		}
+
+		route = routes[pathPart];
+		runList = [ route.before, route.on ].filter(Boolean);
+		runList.after = route.after;
+		runList.parentPath = parentPath;
+	}
+
+	return runList;
 }
 
 Router.prototype.invoke = function(array){
